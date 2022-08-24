2a258fd4027441b8a2511675ebb4ce231f759ed8
Ayesha Usman <ayeshausman650@gmail.com git config --global user.name Ayesha>
2022-08-24 15:10:07 +0500

    Movie Land React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./components/MovieCard";
+import someMovies from "./someMovies";
+
+const API_URL = "https://www.omdbapi.com/?apikey=916312a6";
 
 function App() {
+  // using someMovies array so that page dont show no movies found wheneer page reloads cuz initially movies array is empty;
+  const [movies, setMovies] = useState(someMovies);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const movieSearch = async (title) => {
+    let response = await fetch(`${API_URL}&s=${title}`);
+    let data = await response.json();
+    // console.log(data);
+    setMovies(data.Search);
+  };
+
+  // useEffect(() => {
+  //   movieSearch("movies");
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div
+        className="search"
+        onKeyDown={(e) => e.key === "Enter" && movieSearch(searchTerm)}
+      >
+        <input
+          value={searchTerm}
+          placeholder="Search for a movie"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => movieSearch(searchTerm)}
+        />
+      </div>
+      {/*? won't give an error if movies array is undefined; */}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>no movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/someMovies.js
@@ -0,0 +1,60 @@
+const someMovies = [
+    {
+        "Title": "The Hangover",
+        "Year": "2009",
+        "imdbID": "tt1119646",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNGQwZjg5YmYtY2VkNC00NzliLTljYTctNzI5NmU3MjE2ODQzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Conjuring",
+        "Year": "2013",
+        "imdbID": "tt1457767",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Batman v Superman: Dawn of Justice",
+        "Year": "2016",
+        "imdbID": "tt2975590",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Avengers: Endgame",
+        "Year": "2019",
+        "imdbID": "tt4154796",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+    },
+    {
+        "Title": "365 Days: This Day",
+        "Year": "2022",
+        "imdbID": "tt12996154",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYjY2NzUxNjgtNjJhNy00NTA4LTlmNzItYzQ4MDdjNWYxZjkwXkEyXkFqcGdeQXVyMTEwMTY3NDI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Parasite",
+        "Year": "2019",
+        "imdbID": "tt6751668",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Squid Game",
+        "Year": "2021–",
+        "imdbID": "tt10919420",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYWE3MDVkN2EtNjQ5MS00ZDQ4LTliNzYtMjc2YWMzMDEwMTA3XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg"
+    },
+    {
+        "Title": "Saw",
+        "Year": "2004",
+        "imdbID": "tt0387564",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BM2M1MzI1MWYtYmM2ZC00OWY3LTk0ZGMtNmRkNzU1NzEzMWE5XkEyXkFqcGdeQXVyODUwOTkwODk@._V1_SX300.jpg"
+    }
+];
+
+export default someMovies;
